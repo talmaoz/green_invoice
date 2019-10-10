63b5a500e9a9e79c43a87c27af907667f107fa09
Tal Maoz <tal.maoz.work@gmail.com>
2019-10-10 10:56:04 +0300

    WIP 3

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="login">
+  <section class="login_page">
 
     <div class="left">
       <div class="center">
@@ -8,7 +8,42 @@
     </div>
 
     <div class="right">
-      <img :src="require('@/assets/green_logo.svg')" alt="Green Invoice Logo"/>
+      <header>
+        <img :src="require('@/assets/green_logo.svg')" alt="Green Invoice Logo"/>
+      </header>
+
+      <div class="login  contained">
+        <p class="login_title">התחברות לחשבונית ירוקה</p>
+        <div class="login_form">
+          <div class="login_form_input">
+            <p class="login_form_input_label">מייל</p>
+            <p class="login_form_input_info">כתובת המייל איתה נרשמת לחשבונית ירוקה</p>
+          </div>
+          <div class="login_form_input">
+            <p class="login_form_input_label">סיסמה</p>
+            <a class="login_form_input_info">שכחת סיסמא?</a>
+          </div>
+        </div>
+        <div class="login_buttons">
+          <button class="login_buttons_username_login">כניסה</button>
+          <button class="login_buttons_google_login">כניסה עם גוגל</button>
+        </div>
+      </div>
+
+      <footer class="app_footer contained">
+        <div class="app_footer_right">
+          <div class="app_footer_right_rights_reserved">
+            <img :src="require('@/assets/green_leaf.svg')" alt="Green Invoice Logo"/>
+            <p>חשבונית ירוקה C 2019</p>
+          </div>
+
+          <p class="app_footer_right_company_description">הפקת חשבוניות ומסמכים עסקיים באינטרנט</p>
+        </div>
+
+        <a class="app_footer_left">להצטרפות חינם</a>
+
+
+      </footer>
     </div>
 
   </section>
@@ -39,7 +74,7 @@
 
   @import "@/scss/_variables.scss";
 
-  .login {
+  .login_page {
 
     & > div {
       width: 50%;
@@ -71,6 +106,77 @@
 
     .right {
       float: right;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+
+      header {
+        img {
+          float: right;
+          width: 24%;
+          margin-top: 40px;
+          margin-right: 4.5%;
+          margin-left: 71.5%;
+        }
+      }
+
+      .login {
+
+        &_title {
+
+        }
+
+        &_form {
+
+          &_input {
+            &_label {
+
+            }
+
+            &_info {
+
+            }
+          }
+        }
+
+        &_buttons {
+          &_username_login {
+          }
+
+          &_google_login {
+          }
+        }
+      }
+
+      .app_footer {
+
+        &_right {
+
+          &_rights_reserved {
+            display: flex;
+            flex-direction: row-reverse;
+
+            img {
+              height: 16px;
+              width: 16px;
+              margin-left: 3px;
+            }
+
+            p {
+              margin: 0;
+              display: inline-block;
+            }
+          }
+
+          &_company_description {
+            text-align: right;
+            margin-right: 3px;
+          }
+        }
+
+        &_left {
+        }
+      }
     }
   }
 
