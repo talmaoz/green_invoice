ee1a3764db8a076ffa4d06c4876d4d8a5c5c03b6
Tal Maoz <tal.maoz.work@gmail.com>
2019-10-16 11:57:55 +0300

    adding dist

---
+++ dist/precache-manifest.21c7f0c9ec653a55e4ff4f645d68f7fe.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "65fd0f8d92786be6f21571a3a2ee5a65",
+    "url": "/green_invoice/img/green_logo.65fd0f8d.svg"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/green_invoice/robots.txt"
+  },
+  {
+    "revision": "6134d32866466f2dcec7",
+    "url": "/green_invoice/js/chunk-vendors.b02d9bb2.js"
+  },
+  {
+    "revision": "52152fa5b6d6cea02442",
+    "url": "/green_invoice/js/app.0ba5291b.js"
+  },
+  {
+    "revision": "01057a5c23c65cac1be455971beea331",
+    "url": "/green_invoice/index.html"
+  },
+  {
+    "revision": "03945da8c7ee01960d0002ea9039a698",
+    "url": "/green_invoice/img/green_login_page.03945da8.svg"
+  },
+  {
+    "revision": "b024df93c5127a73b11e1d7c4fdc2e25",
+    "url": "/green_invoice/img/green_leaf.b024df93.svg"
+  },
+  {
+    "revision": "5f6ff37d7e5888ce6a08434e423595bf",
+    "url": "/green_invoice/img/Google_G.5f6ff37d.svg"
+  },
+  {
+    "revision": "b024df93c5127a73b11e1d7c4fdc2e25",
+    "url": "/green_invoice/green_leaf.svg"
+  },
+  {
+    "revision": "da1878f341bb6c256877da123658d272",
+    "url": "/green_invoice/favicon.png"
+  },
+  {
+    "revision": "52152fa5b6d6cea02442",
+    "url": "/green_invoice/css/app.8067d810.css"
+  }
+];
